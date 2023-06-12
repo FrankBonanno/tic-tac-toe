fb93d7d6f618c49a2d8648a38468d8849bd10e3b
FrankBonanno <fb0023@mix.wvu.edu>
2023-06-12 19:43:54 -0400

    player selection

--- src/components/PlayerSelect.jsx
+++ src/components/PlayerSelect.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import XLogo from '../assets/icon-x.svg';
 import OLogo from '../assets/icon-o.svg';
 
 // Styles in Menu.css
 
 const PlayerSelect = () => {
+  const [player, setPlayer] = useState('X');
+  console.log('player', player);
+
+  const switchPlayer = () => {
+    setPlayer(player === 'X' ? 'O' : 'X');
+  };
+
   return (
     <div className="player-select ">
-      <button className="player-select-btn">
+      <button className={`player-select-btn ${player === 'X' ? 'player-selected' : ''}`} onClick={() => switchPlayer()}>
         <img src={XLogo} className="player-selection" />
       </button>
-      <button className="player-select-btn player-selected">
+      <button className={`player-select-btn ${player === 'O' ? 'player-selected' : ''}`} onClick={() => switchPlayer()}>
         <img src={OLogo} className="player-selection" />
       </button>
     </div>
