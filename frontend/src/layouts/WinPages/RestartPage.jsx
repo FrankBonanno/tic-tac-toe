0e912b87592fd83df2e7fe131d721bfdb56d70e3
FrankBonanno <fb0023@mix.wvu.edu>
2023-06-21 23:58:55 -0400

    fixed small gold btn

--- frontend/src/layouts/WinPages/RestartPage.jsx
+++ frontend/src/layouts/WinPages/RestartPage.jsx
@@ -8,7 +8,7 @@ const RestartPage = () => {
         <h3 className="heading-lg">Restart Game?</h3>
         <div className="restart-btns">
           <button className="btn-sm btn-silver heading-xs">No, Cancel</button>
-          <button className="btn-sm btn-gold heading-xs">Yes, Restart</button>
+          <button className="btn-sm btn-gold-sm heading-xs">Yes, Restart</button>
         </div>
       </div>
     </Popup>
