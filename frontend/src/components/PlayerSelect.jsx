b8a36ed583f70df85c0687bc59fa6a1145d18759
FrankBonanno <fb0023@mix.wvu.edu>
2023-07-02 20:52:34 -0400

    strict removed

--- frontend/src/components/PlayerSelect.jsx
+++ frontend/src/components/PlayerSelect.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import XLogo from '../assets/icon-x.svg';
 import OLogo from '../assets/icon-o.svg';
 import { TicTacToeContext } from '../contexts/TicTacToeContext';
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,7 @@ import { TicTacToeProvider } from './contexts/TicTacToeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <TicTacToeProvider>
-      <App />
-    </TicTacToeProvider>
-  </React.StrictMode>
+  <TicTacToeProvider>
+    <App />
+  </TicTacToeProvider>
 );
