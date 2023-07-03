a5036af3fc34d3281b623f796025c54f2fd91c40
FrankBonanno <fb0023@mix.wvu.edu>
2023-07-02 20:35:03 -0400

    I think finished!

--- frontend/src/layouts/WinPages/DrawPage.jsx
+++ frontend/src/layouts/WinPages/DrawPage.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import Popup from '../../components/Popup/Popup';
+import { TicTacToeContext } from '../../contexts/TicTacToeContext';
 
 const DrawPage = () => {
+  const [isRestarting, setIsRestarting] = useState(false);
+  const { resetGame, setMode, resetScores } = useContext(TicTacToeContext);
+
   return (
-    <div>
-      <h1>DrawPage</h1>
-    </div>
+    <Popup>
+      <div className="restart-ctx">
+        <h3 className="heading-lg">Round Tied</h3>
+        <div className="restart-btns">
+          <button
+            className="btn-sm btn-silver heading-xs"
+            onClick={() => {
+              resetGame();
+              setMode('');
+              resetScores();
+            }}
+          >
+            quit
+          </button>
+          <button
+            className="btn-sm btn-gold-sm heading-xs"
+            onClick={() => {
+              resetGame();
+              setIsRestarting(false);
+            }}
+          >
+            Next Round
+          </button>
+        </div>
+      </div>
+    </Popup>
   );
 };
 
